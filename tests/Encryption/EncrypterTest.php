bd9bbebc14208ae9c5f68119d2f4f28ddb8dddb0
Kyle Thielk <kylethielk@gmail.com>
2021-01-29 15:18:05 -0600

    Move Encrypter to OpenSSL and remove mcrypt checks.
    Thanks to: https://github.com/neoxia/laravel-openssl-encryption

--- tests/Encryption/EncrypterTest.php
+++ tests/Encryption/EncrypterTest.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Encryption\Encrypter;
 
+/**
+ * Class EncrypterTest
+ * Modified by TimeToPet. Tests borrowed from https://github.com/neoxia/laravel-openssl-encryption
+ */
 class EncrypterTest extends PHPUnit_Framework_TestCase {
 
 	public function testEncryption()
@@ -16,26 +20,37 @@ class EncrypterTest extends PHPUnit_Framework_TestCase {
 	public function testEncryptionWithCustomCipher()
 	{
 		$e = $this->getEncrypter();
-		$e->setCipher(MCRYPT_RIJNDAEL_256);
+		$e->setCipher('aes-128');
 		$this->assertNotEquals('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', $e->encrypt('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'));
 		$encrypted = $e->encrypt('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
 		$this->assertEquals('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', $e->decrypt($encrypted));
 	}
 
-	/**
-	 * @expectedException Illuminate\Encryption\DecryptException
-	 * @expectedExceptionMessage Invalid data.
-	 */
-	public function testExceptionThrownWhenPayloadIsInvalid()
-	{
-		$e = $this->getEncrypter();
-		$payload = $e->encrypt('foo');
-		$payload = str_shuffle($payload);
-		$e->decrypt($payload);
-	}
+    /**
+     * @expectedException Illuminate\Encryption\DecryptException
+     */
+    public function testExceptionThrownWhenPayloadIsInvalid()
+    {
+        $e = $this->getEncrypter();
+        $payload = $e->encrypt('foo');
+        $payload .= 'adslkadlf';
+        $e->decrypt($payload);
+    }
+
+    /**
+     * @expectedException Illuminate\Encryption\DecryptException
+     */
+    public function testExceptionThrownWhenValueIsInvalid()
+    {
+        $e = $this->getEncrypter();
+        $payload = $e->encrypt('foo');
+        $payload .= 'adlkasdf';
+        $e->decrypt($payload);
+    }
 
 
-	protected function getEncrypter()
+
+    protected function getEncrypter()
 	{
 		return new Encrypter(str_repeat('a', 32));
 	}
